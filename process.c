c507598f723b3708666d9ae2b9a2ec3dea5251e2
Taehee Park <taehpark@c8r3s5.42seoul.kr>
2021-04-14 19:31:54 +0900

    process

--- process.c
+++ process.c
@@ -42,6 +42,100 @@ int		**ft_make_num(char **array, char *charset, int length, int row_num)
 	return (arr_int);
 }
 
+int		find_min(int a, int b, int c)
+{
+	int m;
+
+	m = a;
+	if (m > b)
+		m = b;
+	if (m > c)
+		m = c;
+	return (m);
+}
+
+void	find_bsq(char **array, int length, int row_num)
+{
+	int row;
+	int col;
+
+	row = 0;
+	while (row < row_num)
+	{
+		col = 0;
+		while (col < length)
+		{
+			if (row > 0 && col > 0)
+			{
+				if (array[row][col] != 0 && arr[row-1][col] != 0 &&
+						arr[row-1][col-1] != 0 && arr[row][col-1] != 0)
+					arr[row][col] = min(arr[row-1][col], arr[row-1][col], arr[row-1][col]) + 1;
+			}
+			col++;
+		}
+		row++;
+	}
+}
+
+int		find_maxnum_in_array(char **array, int length, int row_num)
+{
+	int row;
+	int col;
+	int maxnum;
+
+	maxnum = 0;
+	row = 0;
+	while (row < row_num)
+	{
+		col = 0;
+		while (col < length)
+		{
+			if (maxnum < arr[row][col])
+				max = arr[row][col];
+			col++;
+		}
+		row++;
+	}
+	return (maxnum);
+}
+
+int		check_maxnum_index(char **array, int length, int row_num, int maxnum)
+{
+	int maxrow;
+	int maxnum_index[2];
+
+	maxrow =
+
+	maxnum[0] = -1;
+	rownum[1] = -1;
+	while (row < row_num)
+	{
+		col = 0;
+		while (col < length)
+		{
+			if (arr[row][col] == maxnum)
+			{
+				if (maxrow == -1)
+				{
+					maxrow = row;
+					maxcol = col;
+				}
+				else
+				{
+					if (maxrow + maxcol > row + col)
+					{
+						maxrow = row;
+						maxcol = col;
+					}
+				}
+			}
+			col++;
+		}
+		row++;
+	}
+	return (
+}
+
 void	ft_process(char **array, char *charset, int length, int row_num)
 {
 	int		**arr_int;
